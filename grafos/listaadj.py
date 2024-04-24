ff9a02fec36dcfcd35ce86b6fb235bf3d3738da0
Caio Lemos <caioledan@gmail.com>
2024-04-24 20:02:23 -0300

    Lista de Adjacência

---
+++ grafos/listaadj.py
@@ -0,0 +1,32 @@
+def lista_de_adjacencia(arquivo : str) -> dict:
+
+    with open(arquivo, "r") as arquivo:
+        lista = arquivo.readlines()
+
+    listaDeAdjacencia = {}
+    for linha in lista:
+        dados = linha.split(',')
+
+        if dados[0] not in listaDeAdjacencia:
+            listaDeAdjacencia[dados[0]] = []
+        if dados[1] not in listaDeAdjacencia:
+            listaDeAdjacencia[dados[1]] = []
+
+        # if (dados[0] != dados[1]):
+            listaDeAdjacencia[dados[0]].append((dados[1],dados[2]))
+            listaDeAdjacencia[dados[1]].append((dados[0],dados[2]))
+        else:
+            listaDeAdjacencia[dados[1]].append((dados[0],dados[2]))
+
+    for i in listaDeAdjacencia.keys():
+        listaDeAdjacencia[i].sort()
+
+    # return listaDeAdjacencia
+    arq = open("data\\teste.txt","w+")
+
+    arq.write(str(listaDeAdjacencia))
+
+
+lista_de_adjacencia("data\\datavalorada.txt")
+
+        
