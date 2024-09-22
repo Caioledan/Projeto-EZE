93197ca9b27f1d5a90454e080b993ef4aaa1c76b
Guilherme Silva Sales <guilhermessales11@gmail.com>
2024-09-22 17:26:27 -0300

    finalizando sistema de arquivos

--- utils/Utils.py
+++ utils/Utils.py
@@ -3,7 +3,9 @@ from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
 from utils.OSMHandlerConvert import OSMHandler
-from utils.desenhos import draw_buildings_as_cubes, draw_map_with_depth
+from utils.desenhos import draw_buildings_as_cubes, draw_map_with_depth, draw_path
+from utils.PathFinder import PathFinder
+from utils.randomico import Randomic
 
 zoom = 1  
 class Utils(): 
@@ -25,7 +27,7 @@ class Utils():
         gluPerspective(90, 800 / 600, 0.1, 100.0)  # Ângulo de visão, proporção e profundidade ajustados
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
-        gluLookAt(0, 0, zoom, 0, 0, 0, 0, 1, 0)
+        gluLookAt(0, 0, self.zoom, 0, 0, 0, 0, 1, 0)
         glEnable(GL_DEPTH_TEST)
 
     def read_osm(self, filename):
@@ -37,7 +39,7 @@ class Utils():
         lons = [coord[1] for coord in handler.nodes.values()]
         bbox = (min(lats), min(lons), max(lats), max(lons))
 
-        return handler.nodes, handler.ways, handler.buildings, bbox
+        return handler.nodes, handler.ways, handler.buildings, handler.graph, bbox
 
     # Função principal
     def main(self):
@@ -51,16 +53,20 @@ class Utils():
         # Configura a visualização 3D
 
         # Lê o arquivo OSM
-        filename = "./data/edificios2.osm"  # Substitua pelo caminho do arquivo .osm
-        nodes, ways, buiding, bbox = self.read_osm(filename)
+        filename = "data/map.osm"  # Substitua pelo caminho do arquivo .osm
+        nodes, ways, buiding, graph, bbox = self.read_osm(filename)
 
-        # Variáveis de controle de rotação, zoom e posição do mapa
-        rotation_x = 0
-        rotation_y = 0
-        zoom = 1  # Zoom inicial (mais perto do que antes)
-        move_x = 0  # Controle de movimentação no eixo X
-        move_y = 0  # Controle de movimentação no eixo Y
-        move_speed = 0.1  # Velocidade de movimento do mapa
+        draw_min = False
+
+        random = Randomic()
+
+        pathfinder = PathFinder(graph, nodes)
+        n1, n2 = random.randomizer()
+        start_node = int(n1)
+        end_node = int(n2)
+
+
+        path = pathfinder.find_shortest_path(start_node, end_node)
 
         # Loop principal do pygame
         running = True
@@ -76,43 +82,49 @@ class Utils():
             # Controle de rotação, zoom e movimento com o teclado
             keys = pygame.key.get_pressed()
             if keys[K_LEFT]:
-                rotation_y -= 1
+                self.rotation_y -= 1
             if keys[K_RIGHT]:
-                rotation_y += 1
+                self.rotation_y += 1
             if keys[K_UP]:
-                rotation_x -= 1
+                self.rotation_x -= 1
             if keys[K_DOWN]:
-                rotation_x += 1
+                self.rotation_x += 1
             if keys[K_EQUALS] or keys[K_PLUS]:  # Tecla '+' para aumentar o zoom
-                print("Zoom: ", zoom)
-                zoom -= 0.1  # Aproxima a câmera
+                self.zoom -= 0.1  # Aproxima a câmera
             if keys[K_MINUS]:  # Tecla '-' para diminuir o zoom
-                print("Zoom: ", zoom)
-                zoom += 0.1  # Afasta a câmera
+                self.zoom += 0.1  # Afasta a câmera
 
             # Movimentação do mapa com as teclas W, A, S, D
             if keys[K_w]:
-                move_y -= move_speed  # Move o mapa para cima
+                self.move_y -= self.move_speed  # Move o mapa para cima
             if keys[K_s]:
-                move_y += move_speed  # Move o mapa para baixo
+                self.move_y += self.move_speed  # Move o mapa para baixo
             if keys[K_a]:
-                move_x -= move_speed  # Move o mapa para a esquerda
+                self.move_x += self.move_speed  # Move o mapa para a esquerda
             if keys[K_d]:
-                move_x += move_speed  # Move o mapa para a direita
+                self.move_x -= self.move_speed  # Move o mapa para a direita
 
 
             # Aplica rotação no mapa
-            glRotatef(rotation_x, 1, 0, 0)  # Rotaciona em torno do eixo X
-            glRotatef(rotation_y, 0, 1, 0)  # Rotaciona em torno do eixo Y
+            glRotatef(self.rotation_x, 1, 0, 0)  # Rotaciona em torno do eixo X
+            glRotatef(self.rotation_y, 0, 1, 0)  # Rotaciona em torno do eixo Y
 
             # Aplica movimentação no mapa
-            glTranslatef(move_x, move_y, 0)  # Move o mapa para cima/baixo e esquerda/direita
+            glTranslatef(self.move_x, self.move_y, 0)  # Move o mapa para cima/baixo e esquerda/direita
             
             draw_buildings_as_cubes(nodes, buiding, bbox)
             # Desenha o mapa com profundidade
             draw_map_with_depth(nodes, ways, bbox)
 
+            if keys[K_SPACE]:
+                draw_min = True
 
+            if draw_min == True:
+                glDisable(GL_DEPTH_TEST)
+                draw_path(nodes, path, bbox)
+                glEnable(GL_DEPTH_TEST)
+
+        
             pygame.display.flip()
             pygame.time.wait(10)
 
--- utils/desenhos.py
+++ utils/desenhos.py
@@ -66,6 +66,35 @@ def draw_map_with_depth(nodes, ways, bbox, street_width=0.001, depth=0.001):
 
 # Função para desenhar prédios com profundidade (como prismas)
 # Função para desenhar prédios no formato de cubos
+# Função para desenhar o caminho em rosa
+def draw_path(nodes, path, bbox, street_width=0.0010, depth=0.0001):
+    glColor3f(40.0/255.0, 224.0/255.0, 254.0/255.0)  # Cor rosa
+    glBegin(GL_QUADS)
+
+    for i in range(len(path) - 1):
+        node1 = nodes[path[i]]
+        node2 = nodes[path[i + 1]]
+        x1, y1, z1 = latlon_to_opengl(node1[0], node1[1], bbox, z=0)
+        x2, y2, z2 = latlon_to_opengl(node2[0], node2[1], bbox, z=0)
+        direction = np.array([x2 - x1, y2 - y1])
+        direction = direction / np.linalg.norm(direction)
+        perpendicular = np.array([-direction[1], direction[0]]) * street_width
+        v1 = [x1 + perpendicular[0], y1 + perpendicular[1], z1]
+        v2 = [x1 - perpendicular[0], y1 - perpendicular[1], z1]
+        v3 = [x2 + perpendicular[0], y2 + perpendicular[1], z2]
+        v4 = [x2 - perpendicular[0], y2 - perpendicular[1], z2]
+        glVertex3f(v1[0], v1[1], z1 + depth)
+        glVertex3f(v2[0], v2[1], z1 + depth)
+        glVertex3f(v4[0], v4[1], z2 + depth)
+        glVertex3f(v3[0], v3[1], z2 + depth)
+        glVertex3f(v1[0], v1[1], z1)
+        glVertex3f(v2[0], v2[1], z1)
+        glVertex3f(v4[0], v4[1], z2)
+        glVertex3f(v3[0], v3[1], z2)
+    glEnd()
+
+
+
 def draw_buildings_as_cubes(nodes, buildings, bbox, building_width=0.0001, building_height=0.001):
     glColor3f(0.7, 0.7, 0.7)  # Cor cinza para os prédios
     glBegin(GL_QUADS)  # Usamos quadrados para desenhar as faces
@@ -125,71 +154,5 @@ def draw_buildings_as_cubes(nodes, buildings, bbox, building_width=0.0001, build
             glVertex3f(vertices[next_i][0] - building_width/2, vertices[next_i][1] + building_width/2, 0)
             glVertex3f(vertices[next_i][0] - building_width/2, vertices[next_i][1] + building_width/2, building_height)
             glVertex3f(vertices[i][0] - building_width/2, vertices[i][1] + building_width/2, building_height)
-
-    glEnd()
-
-
-
-# Função para desenhar casas (cubos/prismas) ao lado das ruas
-def draw_buildings(nodes, ways, bbox, building_width=0.0001, building_height=0.0001):
-    glColor3f(0.7, 0.7, 0.7)  # Cor cinza para os prédios
-    glBegin(GL_QUADS)
-    
-    for way in ways:
-        for i in range(len(way) - 1):
-            # Pega os dois nós consecutivos que representam uma rua
-            node1 = nodes[way[i]]
-            node2 = nodes[way[i + 1]]
-
-            # Converte as coordenadas lat/lon em coordenadas OpenGL
-            x1, y1, z1 = latlon_to_opengl(node1[0], node1[1], bbox, z=0)
-            x2, y2, z2 = latlon_to_opengl(node2[0], node2[1], bbox, z=0)
-
-            # Vetor de direção da rua (vetor entre os dois nós)
-            direction = np.array([x2 - x1, y2 - y1])
-            direction = direction / np.linalg.norm(direction)  # Normaliza o vetor
-
-            # Vetor perpendicular à rua (para posicionar os edifícios ao lado das ruas)
-            perpendicular = np.array([-direction[1], direction[0]]) * building_width
-
-            # Calcula os quatro vértices do prédio ao lado da rua
-            b1 = [x1 + perpendicular[0], y1 + perpendicular[1], z1]
-            b2 = [x1 - perpendicular[0], y1 - perpendicular[1], z1]
-            b3 = [x2 + perpendicular[0], y2 + perpendicular[1], z2]
-            b4 = [x2 - perpendicular[0], y2 - perpendicular[1], z2]
-
-            # Desenha o prédio como um prisma com altura variável
-            for i in range(2):  # Desenha dois prédios (um de cada lado da rua)
-                # Desloca o prédio para longe da rua no lado perpendicular
-                offset = perpendicular * (i * 2 - 1)  # Alterna entre os dois lados
-
-                # Vértices superiores do prédio
-                v1 = [b1[0] + offset[0], b1[1] + offset[1], z1 + building_height]
-                v2 = [b2[0] + offset[0], b2[1] + offset[1], z1 + building_height]
-                v3 = [b4[0] + offset[0], b4[1] + offset[1], z2 + building_height]
-                v4 = [b3[0] + offset[0], b3[1] + offset[1], z2 + building_height]
-
-                # Face superior
-                glVertex3f(v1[0], v1[1], v1[2])
-                glVertex3f(v2[0], v2[1], v2[2])
-                glVertex3f(v3[0], v3[1], v3[2])
-                glVertex3f(v4[0], v4[1], v4[2])
-
-                # Face inferior
-                glVertex3f(b1[0] + offset[0], b1[1] + offset[1], z1)
-                glVertex3f(b2[0] + offset[0], b2[1] + offset[1], z1)
-                glVertex3f(b4[0] + offset[0], b4[1] + offset[1], z2)
-                glVertex3f(b3[0] + offset[0], b3[1] + offset[1], z2)
-
-                # Laterais do prédio
-                glVertex3f(v1[0], v1[1], v1[2])
-                glVertex3f(v1[0], v1[1], z1)
-                glVertex3f(v4[0], v4[1], z2)
-                glVertex3f(v4[0], v4[1], v4[2])
-
-                glVertex3f(v2[0], v2[1], v2[2])
-                glVertex3f(v2[0], v2[1], z1)
-                glVertex3f(v3[0], v3[1], z2)
-                glVertex3f(v3[0], v3[1], v3[2])
 
     glEnd()
