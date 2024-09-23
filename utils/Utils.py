04108734e0e63e7c6afbb55a26eb90265780cdec
Guilherme Silva Sales <guilhermessales11@gmail.com>
2024-09-23 19:11:46 -0300

    GLUT Window

--- utils/Utils.py
+++ utils/Utils.py
@@ -1,36 +1,36 @@
-import pygame
-from pygame.locals import *
 from OpenGL.GL import *
 from OpenGL.GLU import *
+from OpenGL.GLUT import *
+import glm
 from utils.OSMHandlerConvert import OSMHandler, latlon_to_opengl
 from utils.desenhos import draw_buildings_as_cubes, draw_map_with_depth, draw_path
 from utils.PathFinder import PathFinder
 from utils.randomicc import Randomic
 from utils.Carro import *
 
-zoom = 1  
-class Utils(): 
+class Utils():
     def __init__(self):
-
-        # Variaveis globais
+        # Variáveis globais
         self.rotation_x = 0
         self.rotation_y = 0
-        self.zoom = 1  # Zoom inicial (mais perto do que antes)
-        self.move_x = 0  # Controle de movimentação no eixo X
-        self.move_y = 0  # Controle de movimentação no eixo Y
-        self.move_speed = 0.1  # Velocidade de movimento do mapa
-        self.posicao = glm.vec3(0,0,0) #posição do carro
-        self.direcao = glm.vec3(0,1,0) #vetor direção no eixo y
-        self.lateral = glm.vec3(1,0,0) #vetor lateral no eixo x
+        self.zoom = 1
+        self.move_x = 0
+        self.move_y = 0
+        self.move_speed = 0.1
+        self.posicao = glm.vec3(0, 0, 0)  # Posição do carro
+        self.direcao = glm.vec3(0, 1, 0)  # Vetor direção no eixo y
+        self.lateral = glm.vec3(1, 0, 0)  # Vetor lateral no eixo x
         self.trajeto = []
+        self.draw_min = False
+        self.carro = None
+        self.nodes = None
+        self.bbox = None
+        self.path = None
 
-
-
-    # Função para configurar o modo de perspectiva 3D
     def setup_3d_view(self):
         glMatrixMode(GL_PROJECTION)
         glLoadIdentity()
-        gluPerspective(90, 800 / 600, 0.1, 100.0)  # Ângulo de visão, proporção e profundidade ajustados
+        gluPerspective(90, 800 / 600, 0.1, 100.0)
         glMatrixMode(GL_MODELVIEW)
         glLoadIdentity()
         gluLookAt(0, 0, self.zoom, 0, 0, 0, 0, 1, 0)
@@ -39,138 +39,102 @@ class Utils():
     def read_osm(self, filename):
         handler = OSMHandler()
         handler.apply_file(filename)
-
-        # Encontrar os limites geográficos (bounding box)
         lats = [coord[0] for coord in handler.nodes.values()]
         lons = [coord[1] for coord in handler.nodes.values()]
         bbox = (min(lats), min(lons), max(lats), max(lons))
-
         return handler.nodes, handler.ways, handler.buildings, handler.graph, bbox
-    
-    def create_path(self, nodes, path, bbox):
-        
 
+    def create_path(self, nodes, path, bbox):
         for i in range(len(path) - 1):
             node1 = nodes[path[i]]
             node2 = nodes[path[i + 1]]
-
             x1, y1, z1 = latlon_to_opengl(node1[0], node1[1], bbox, z=0)
             x2, y2, z2 = latlon_to_opengl(node2[0], node2[1], bbox, z=0)
+            self.trajeto.append(glm.vec3(x1, y1, z1))
+            self.trajeto.append(glm.vec3(x2, y2, z2))
 
-            self.trajeto.append(glm.vec3(x1,y1,z1))
-            self.trajeto.append(glm.vec3(x2,y2,z2))
+    def keyboard_callback(self, key, x, y):
+        # Controle de rotação, zoom e movimento
+        if key == b'\x1b':  # Tecla ESC para sair
+            glutLeaveMainLoop()
+        elif key == b'w':
+            self.move_y -= self.move_speed
+        elif key == b's':
+            self.move_y += self.move_speed
+        elif key == b'a':
+            self.move_x += self.move_speed
+        elif key == b'd':
+            self.move_x -= self.move_speed
+        elif key == b'=':
+            self.zoom -= 0.1
+        elif key == b'-':
+            self.zoom += 0.1
+        elif key == b' ':
+            self.draw_min = not self.draw_min
 
+    def special_callback(self, key, x, y):
+        if key == GLUT_KEY_LEFT:
+            self.rotation_y -= 1
+        elif key == GLUT_KEY_RIGHT:
+            self.rotation_y += 1
+        elif key == GLUT_KEY_UP:
+            self.rotation_x -= 1
+        elif key == GLUT_KEY_DOWN:
+            self.rotation_x += 1
 
+    def display_callback(self):
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+        self.setup_3d_view()
+        glRotatef(self.rotation_x, 1, 0, 0)
+        glRotatef(self.rotation_y, 0, 1, 0)
+        glTranslatef(self.move_x, self.move_y, 0)
 
-    # Função principal
+        # Desenha prédios e mapa
+        draw_buildings_as_cubes(self.nodes, self.buiding, self.bbox)
+        draw_map_with_depth(self.nodes, self.ways, self.bbox)
+
+        # Desenha o caminho se a tecla espaço foi apertada
+        if self.draw_min:
+            glDisable(GL_DEPTH_TEST)
+            draw_path(self.nodes, self.path, self.bbox)
+            glEnable(GL_DEPTH_TEST)
+
+        # Desenha o carro
+        glLineWidth(0.01)
+        glPushMatrix()
+        glScale(0.005, 0.005, 0.005)
+        self.carro.desenhar()
+        glPopMatrix()
+
+        glutSwapBuffers()
+
     def main(self):
-        global rotation_x, rotation_y, zoom, move_x, move_y, move_speed
-        # Inicializa pygame
-        pygame.init()
-        display = (1080, 920)
-        pygame.display.set_mode(display, DOUBLEBUF | OPENGL)
+        # Inicializa GLUT
+        glutInit()
+        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB | GLUT_DEPTH)
+        glutInitWindowSize(1080, 920)
+        glutCreateWindow(b"WAZE PROJECT")
+        glClearColor(39.0 / 255.0, 45.0 / 255.0, 57.0 / 255.0, 1.0)
 
-        glClearColor(39.0/255.0, 45.0/255.0, 57.0/255.0, 1.0)  # Cor de fundo azul claro (R, G, B, A)
-        # Configura a visualização 3D
-
-        glOrtho(-1,1,-1,1,-2,2)
-
-        # Lê o arquivo OSM
-        filename = "data/map.osm"  # Substitua pelo caminho do arquivo .osm
-        nodes, ways, buiding, graph, bbox = self.read_osm(filename)
-
-        draw_min = False
-
+        # Configuração do mapa
+        filename = "data/map.osm"
+        self.nodes, self.ways, self.buiding, self.graph, self.bbox = self.read_osm(filename)
         random = Randomic()
-
-        pathfinder = PathFinder(graph, nodes)
+        pathfinder = PathFinder(self.graph, self.nodes)
         n1, n2 = random.randomizer()
         start_node = int(n1)
         end_node = int(n2)
+        self.path = pathfinder.find_shortest_path(start_node, end_node)
+        self.carro = Carro(self.posicao, self.direcao, self.lateral)
+        self.create_path(self.nodes, self.path, self.bbox)
+        self.carro.setarPosicaoInicio(*self.trajeto[0])
+        self.carro.calculaProxDirec(self.trajeto[1])
 
+        # Configurações do GLUT
+        glutDisplayFunc(self.display_callback)
+        glutIdleFunc(self.display_callback)
+        glutKeyboardFunc(self.keyboard_callback)
+        glutSpecialFunc(self.special_callback)
 
-        path = pathfinder.find_shortest_path(start_node, end_node)
-
-
-        carro = Carro(self.posicao, self.direcao, self.lateral)
-
-        self.create_path(nodes,path,bbox)
-
-        vertice = 0
-
-        carro.setarPosicaoInicio(*self.trajeto[vertice])
-        print(carro.posicao)
-        carro.calculaProxDirec(self.trajeto[vertice+1])
-
-
-        # Loop principal do pygame
-        running = True
-        while running:
-            glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)    
-
-            self.setup_3d_view()
-
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-
-            # Controle de rotação, zoom e movimento com o teclado
-            keys = pygame.key.get_pressed()
-            if keys[K_LEFT]:
-                self.rotation_y -= 1
-            if keys[K_RIGHT]:
-                self.rotation_y += 1
-            if keys[K_UP]:
-                self.rotation_x -= 1
-            if keys[K_DOWN]:
-                self.rotation_x += 1
-            if keys[K_EQUALS] or keys[K_PLUS]:  # Tecla '+' para aumentar o zoom
-                self.zoom -= 0.1  # Aproxima a câmera
-            if keys[K_MINUS]:  # Tecla '-' para diminuir o zoom
-                self.zoom += 0.1  # Afasta a câmera
-
-            # Movimentação do mapa com as teclas W, A, S, D
-            if keys[K_w]:
-                self.move_y -= self.move_speed  # Move o mapa para cima
-            if keys[K_s]:
-                self.move_y += self.move_speed  # Move o mapa para baixo
-            if keys[K_a]:
-                self.move_x += self.move_speed  # Move o mapa para a esquerda
-            if keys[K_d]:
-                self.move_x -= self.move_speed  # Move o mapa para a direita
-
-
-            # Aplica rotação no mapa
-            glRotatef(self.rotation_x, 1, 0, 0)  # Rotaciona em torno do eixo X
-            glRotatef(self.rotation_y, 0, 1, 0)  # Rotaciona em torno do eixo Y
-
-            # Aplica movimentação no mapa
-            glTranslatef(self.move_x, self.move_y, 0)  # Move o mapa para cima/baixo e esquerda/direita
-            
-            draw_buildings_as_cubes(nodes, buiding, bbox)
-            # Desenha o mapa com profundidade
-            draw_map_with_depth(nodes, ways, bbox)
-
-            if keys[K_SPACE]: # Caso a tecla espaço seja apertada, um caminho será traçado no mapa
-                draw_min = not draw_min
-                pygame.time.delay(50)
-                
-
-            if draw_min:
-                glDisable(GL_DEPTH_TEST)
-                draw_path(nodes, path, bbox)
-                glEnable(GL_DEPTH_TEST)
-
-
-
-            glLineWidth(0.01)
-            glPushMatrix()
-            glScale(0.005, 0.005,0.005)
-            carro.desenhar()
-            glPopMatrix()
-
-        
-            pygame.display.flip()
-            pygame.time.wait(10)
-
-        pygame.quit()
+        # Loop principal
+        glutMainLoop()
