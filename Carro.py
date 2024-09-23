9413364a6cd584fdafc27e10a280282c1eaa8c1b
Diego Ribeiro <diego09022004@gmail.com>
2024-09-22 23:47:07 -0300

    carro com movimentação no trajeto
    Implementação do algoritmo de movimento do carro junto com uma main para ter noção de como está.

--- Carro.py
+++ Carro.py
@@ -1,26 +1,71 @@
 from OpenGL.GL import *
-from OpenGL.GLUT import *
-from OpenGL.GLU import *
+import glm
 
-v1 = [-1,0,0]
-v2 = [1,0,0]
-v3 = [0,2,0]
-v4 = [-1,0,0.50]
-v5 = [1,0,0.50]
-v6 = [0,2,0.50]
-
-vertices = [v1,v2,v3,v4,v5,v6]
+v1 = [-1,-1,0]
+v2 = [1,-1,0]
+v3 = [0,1,0]
+v4 = [-1,-1,0.50]
+v5 = [1,-1,0.50]
+v6 = [0,1,0.50]
+M = glm.mat4(1)
+velocidade = 0.05
+trajeto = [v1,v2,v3,v4,v5,v6]
 
 class Carro:
-    def __init__(self):
-        global v1,v2,v3,v4,v5,v6
+    def __init__(self, posicao, direcao,lateral):
+        self.posicao = posicao
+        self.direcao = direcao
+        self.lateral = lateral
+        self.velocidade = velocidade
+
+    
+    def calcMatriz(self):
+        global M
+        M[0] = glm.vec4(self.lateral,0) #1 COLUNA VETOR I, lateral do carro
+        M[1] = glm.vec4(self.direcao,0) #2 COLUNA VETOR J, direção do carro
+        M[2] = glm.vec4(0,0,1,0) #3 COLUNA VETOR K, topo do carro eixo z
+        M[3] = glm.vec4(self.posicao,1) #4 COLUNA POSICAO DO CARRO
+
+    def andar(self):
+        self.posicao = self.posicao + velocidade*self.direcao
+
+    def calculaProxDirec(self,vertice):
+        vetorVertCarro = glm.normalize(vertice - self.posicao)#Vetor que sai do carro até o vertice e normalizo ele.
+        escalar = glm.dot(self.direcao,vetorVertCarro) #Vai me voltar o cosseno desses angulos
+        angulo = glm.acos(escalar) #Uso a função
+        print(angulo)
+
+        # Determina o eixo de rotação (assumindo que o carro está no plano XY, o eixo de rotação é Z)
+        eixoRotacao = glm.vec3(0, 0, 1)
+        
+        # Produto vetorial entre a direção atual e o vetor até o vértice
+        produtoVetorial = glm.cross(self.direcao, vetorVertCarro)
+        
+        # Se a componente Z do produto vetorial for negativa, rotacionar para a direita
+        if produtoVetorial.z < 0:
+            angulo = -angulo  # Rotaciona para a direita (sentido horário)
+        
+        # Aplica a rotação da direção e do vetor lateral com base no ângulo e eixo Z
+        self.direcao = glm.normalize(glm.rotate(angulo, eixoRotacao) * self.direcao)
+        self.lateral = glm.normalize(glm.rotate(angulo, eixoRotacao) * self.lateral)
+
+        # if (angulo > 0.50):
+        #     self.direcao = glm.rotate(-angulo) * self.direcao
+        #     self.lateral = glm.rotate(-angulo) * self.lateral
+        # else:
+        #     self.direcao = glm.rotate(angulo) * self.direcao
+        #     self.lateral = glm.rotate(angulo) * self.lateral
+        glm.normalize(self.direcao)
+        glm.normalize(self.lateral)
+        self.calcMatriz()
+        
 
     def desenhar(self):
-        global v1,v2,v3,v4,v5,v6
+        global v1,v2,v3,v4,v5,v6,trajeto
         #Desenhando ao redor do triângulo..
         glColor3f(0,0.5,0.8)
         glBegin(GL_TRIANGLES)
-        for i in vertices:
+        for i in trajeto:
             glVertex3fv(i)
         glEnd()
 
@@ -49,7 +94,7 @@ class Carro:
         glColor3f(1,1,1)
 
         glBegin(GL_TRIANGLES)
-        for i in vertices:
+        for i in trajeto:
             glVertex3fv(i)
         glEnd()
 
---
+++ maintemp.py
@@ -0,0 +1,130 @@
+from OpenGL.GL import *
+from OpenGL.GLUT import *
+from OpenGL.GLU import *
+import glm
+from glfw import get_time
+from Carro import *
+import numpy as np
+
+
+posicao = glm.vec3(0,0,0) #posição do carro
+direcao = glm.vec3(0,1,0) #vetor direção no eixo y
+lateral = glm.vec3(1,0,0) #vetor lateral no eixo x
+
+
+trajeto = [glm.vec3(2,2,0),
+           glm.vec3(4,2,0),
+           glm.vec3(6,2,0),
+           glm.vec3(7,3,0),
+           glm.vec3(6,5,0),
+           glm.vec3(5,6,0)] #percurso
+
+carro = Carro(posicao,direcao,lateral)
+
+
+def desenhapercurso():
+    global trajeto
+    glLineWidth(5)
+    glColor3f(1,0.5,0)
+    glBegin(GL_LINE_STRIP)
+    for i in trajeto:
+        glVertex3f(i.x,i.y,i.z)
+    glEnd()
+   
+
+def desenhaLinhasXeYeZ():
+    glColor3f(1,0,0)
+    glBegin(GL_LINES)
+    #Desenhnado o eixo X
+    glVertex3f(-50,0,0)
+    glVertex3f(50,0,0)
+    #Desenhando o eixo Y
+    glColor3f(0,1,0)
+    glVertex3f(0,50,0)
+    glVertex3f(0,-50,0)
+    #Desenhando o eixo Z
+    glColor3f(0,0,1)
+    glVertex3f(0,0,50)
+    glVertex3f(0,0,-50)
+
+    glEnd()
+
+def inicio():
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    glOrtho(-5,5,-5,5,-5,100) #Largura da projeção
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    gluLookAt(carro.posicao.x,carro.posicao.y-1,1,2,2,0,0,0,1)
+    
+
+    glClearColor(0.5,0.5,0.5,0.5)#Cor do fundo
+    glEnable(GL_DEPTH_TEST)  # Habilita o teste de profundidade
+
+vertice = 0
+carro.calculaProxDirec(trajeto[vertice])
+
+def timer(v):
+    global carro,vertice
+    #a cada frame é necessário chamar essa função para 'agendar' a sua próxima execução
+    glutTimerFunc(int(1000/60), timer, 0)  
+
+    #Atualizando a posição da câmera
+    glMatrixMode(GL_MODELVIEW)
+    glLoadIdentity()
+    #definindo a posição da câmera um pouco atrás e acima do carro
+    gluLookAt(carro.posicao.x-1, carro.posicao.y, 0.7,  
+              *carro.posicao,  
+              0, 0, 1)  
+
+
+
+    if vertice < len(trajeto)-1:
+        if(glm.distance(carro.posicao,trajeto[vertice]) < 0.1): #Ao chegar no vértice, recalcula para o outro.
+            vertice = vertice + 1
+            carro.calculaProxDirec(trajeto[vertice])
+            carro.andar()
+            carro.calcMatriz()
+        else:
+            carro.andar()
+            carro.calcMatriz()
+        
+
+
+    glutPostRedisplay()
+
+
+
+def desenhar():
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) # Limpa o conteÃºdo do frame buffer aplicando a cor usada em glClearColor em toda a imagem
+    
+
+
+
+    desenhaLinhasXeYeZ()
+
+    glPushMatrix()
+    glMultMatrixf(np.asarray(glm.transpose(M))) # função que aplica uma matriz qualquer no objeto
+    carro.desenhar()
+    glPopMatrix()
+
+    desenhapercurso()
+
+
+    # # Cálculo do deltaTime e velocidade
+    # frameAtual = get_time()
+    # deltaTime = frameAtual - ultimoFrame
+    # ultimoFrame = frameAtual
+    # velocidade = 2.5 * deltaTime
+  
+    glutSwapBuffers()
+
+glutInit()
+glutInitWindowSize(500,500)
+glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB)
+glutInitWindowPosition(0,0)
+glutCreateWindow("Prototipo de automovel")
+inicio()
+glutTimerFunc(int(1000/60), timer, 0) # função 'timer' será chamada daqui a 1000/FPS milissegundos
+glutDisplayFunc(desenhar)
+glutMainLoop()
