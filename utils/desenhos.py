8fa2d1cec85b940ae6151226a189f8911d56346f
Caio Lemos <caioledan@gmail.com>
2024-09-23 17:16:56 -0300

    .

--- utils/desenhos.py
+++ utils/desenhos.py
@@ -67,31 +67,24 @@ def draw_map_with_depth(nodes, ways, bbox, street_width=0.001, depth=0.001):
 # Função para desenhar prédios com profundidade (como prismas)
 # Função para desenhar prédios no formato de cubos
 # Função para desenhar o caminho em rosa
-def draw_path(nodes, path, bbox, street_width=0.0010, depth=0.0001):
-    glColor3f(40.0/255.0, 224.0/255.0, 254.0/255.0)  # Cor rosa
-    glBegin(GL_QUADS)
+def draw_path(nodes, path, bbox, street_width=5):
+    glColor3f(40.0/255.0, 224.0/255.0, 254.0/255.0)  # Cor vermelha para o caminho
+    glLineWidth(street_width)  # Largura da linha para destacar o caminho
+    glBegin(GL_LINES)
 
     for i in range(len(path) - 1):
         node1 = nodes[path[i]]
         node2 = nodes[path[i + 1]]
+
         x1, y1, z1 = latlon_to_opengl(node1[0], node1[1], bbox, z=0)
         x2, y2, z2 = latlon_to_opengl(node2[0], node2[1], bbox, z=0)
-        direction = np.array([x2 - x1, y2 - y1])
-        direction = direction / np.linalg.norm(direction)
-        perpendicular = np.array([-direction[1], direction[0]]) * street_width
-        v1 = [x1 + perpendicular[0], y1 + perpendicular[1], z1]
-        v2 = [x1 - perpendicular[0], y1 - perpendicular[1], z1]
-        v3 = [x2 + perpendicular[0], y2 + perpendicular[1], z2]
-        v4 = [x2 - perpendicular[0], y2 - perpendicular[1], z2]
-        glVertex3f(v1[0], v1[1], z1 + depth)
-        glVertex3f(v2[0], v2[1], z1 + depth)
-        glVertex3f(v4[0], v4[1], z2 + depth)
-        glVertex3f(v3[0], v3[1], z2 + depth)
-        glVertex3f(v1[0], v1[1], z1)
-        glVertex3f(v2[0], v2[1], z1)
-        glVertex3f(v4[0], v4[1], z2)
-        glVertex3f(v3[0], v3[1], z2)
+
+        # Desenha uma linha entre os dois nós
+        glVertex3f(x1, y1, z1)
+        glVertex3f(x2, y2, z2)
+
     glEnd()
+
 
 
 
